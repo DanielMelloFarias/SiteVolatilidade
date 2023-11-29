511f17abbaedbdcb5e7a91064c7b9c7e6a7649f3
DanielMelloFarias <danielmellofarias@gmail.com>
2023-11-29 14:16:20 -0300

    create site_volatilidade.py

---
+++ site_volatilidade.py
@@ -0,0 +1,11 @@
+import streamlit as st
+from streamlit_chat import message
+import openai, requests
+import json
+import os
+import random
+import gspread
+import oauth2client
+import datetime
+#from PIL import Image
+
